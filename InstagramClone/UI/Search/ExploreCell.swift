3def119495207d9ccf7c7c0b733483c9ee771738
kadanur <mkadanur@gmail.com>
2022-03-22 19:19:37 +0300

    Profile and Search View Controllers

---
+++ InstagramClone/UI/Search/ExploreCell.swift
@@ -0,0 +1,15 @@
+//
+//  ExploreCell.swift
+//  InstagramClone
+//
+//  Created by M Kaan Adanur on 22.03.2022.
+//
+
+import Foundation
+import UIKit
+
+class ExploreCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    
+}
--- InstagramClone/UI/Search/SearchViewController.swift
+++ InstagramClone/UI/Search/SearchViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+}
+
+extension SearchViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 40
     }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "exploreCell", for: indexPath) as! ExploreCell
+        cell.cellImage.image = UIImage(named: "post1")
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let screenWidth = view.bounds.width
+        let cellWidth = (screenWidth - 2) / 3
+        return CGSize(width: cellWidth, height: cellWidth)
+    }
 }
